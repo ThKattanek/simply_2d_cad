f5b012bbc2ef21e122e696474012881fb0963c23
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2026-08-30 08:14:26 +0200

    Fixed mouse cursor, now not scaling when zooming

--- src/crosshair_item.cpp
+++ src/crosshair_item.cpp
@@ -1,32 +1,59 @@
 #include "crosshair_item.h"
 #include <QPainter>
+#include <QBitmap>
+
+#define CROSSHAIR_SIZE 25   // Size of the crosshair bitmap (25x25 pixels). Must always be an odd number
 
 CrosshairItem::CrosshairItem(QGraphicsItem *parent) : QGraphicsItem(parent)
 {
-    // set the crosshair to be always on top of other items
-    setZValue(1000); // Ensure the crosshair is on top of other items
+    setAcceptedMouseButtons(Qt::NoButton);              // Crosshair should not accept mouse events
+    setFlag(QGraphicsItem::ItemIsSelectable, false);    // Crosshair should not be selectable
 
-    setAcceptedMouseButtons(Qt::NoButton); // Crosshair should not accept mouse events
-    setFlag(QGraphicsItem::ItemIsSelectable, false); // Crosshair should not be selectable
+    // Set the point to be always not affected by transformations (like scaling or rotation)
+    setFlag(QGraphicsItem::ItemIgnoresTransformations, true);
 
-    m_pen = QPen(Qt::green, 0); // Set the pen color to green and width to 0 (cosmetic pen)
+    m_pen = QPen(Qt::white, 0); // Set the pen color to green and width to 0 (cosmetic pen)
 }
 
 QRectF CrosshairItem::boundingRect() const {
     // Sehr großes Bounding-Rechteck um das Fadenkreuz herum
-    return QRectF(m_position.x() - 50000, m_position.y() - 50000, 100000, 100000);
+    return QRectF(((CROSSHAIR_SIZE - 1) / 2) - CROSSHAIR_SIZE, ((CROSSHAIR_SIZE - 1) / 2) - CROSSHAIR_SIZE, CROSSHAIR_SIZE - 1, CROSSHAIR_SIZE - 1);
 }
 
-void CrosshairItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) {
-    painter->setPen(m_pen);
+void CrosshairItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) {    
+    painter->setRenderHint(QPainter::Antialiasing, false);
 
-    // Horizontale & Vertikale Linie
-    painter->drawLine(QPointF(m_position.x() - m_size, m_position.y()), QPointF(m_position.x() + m_size, m_position.y()));
-    painter->drawLine(QPointF(m_position.x(), m_position.y() - m_size), QPointF(m_position.x(), m_position.y() + m_size));
+    painter->setPen(m_pen);
+    painter->drawPixmap(((CROSSHAIR_SIZE - 1) / 2) - CROSSHAIR_SIZE, ((CROSSHAIR_SIZE - 1) / 2) - CROSSHAIR_SIZE, getPatternBitmap());
 }
 
 void CrosshairItem::setPosition(const QPointF &pos) {
     prepareGeometryChange();
     m_position = pos;
+    setPos(pos);
     update();
+}
+
+const QBitmap &CrosshairItem::getPatternBitmap()
+{
+    static std::unique_ptr<QBitmap> s_bitmap = nullptr;
+
+    if (!s_bitmap) {
+
+        // Create a 11x11 bitmap with transparent background
+        auto bitmap = std::make_unique<QBitmap>(CROSSHAIR_SIZE, CROSSHAIR_SIZE);
+        bitmap->fill(Qt::color0); // Fill with transparent color
+
+        // Draw a simple pattern (cross) on the image
+        QPainter p(bitmap.get());
+        p.setPen(QPen(Qt::color1, 1));
+
+        p.drawLine(0, (CROSSHAIR_SIZE - 1) / 2, CROSSHAIR_SIZE - 1, (CROSSHAIR_SIZE - 1) / 2);
+        p.drawLine((CROSSHAIR_SIZE - 1) / 2, 0, (CROSSHAIR_SIZE - 1) / 2, CROSSHAIR_SIZE - 1);
+
+        // Create the pixmap from the image and store it in the static unique_ptr
+        s_bitmap = std::move(bitmap);
+    }
+
+    return *s_bitmap;
 }
--- src/crosshair_item.h
+++ src/crosshair_item.h
@@ -10,12 +10,12 @@ public:
 
     QRectF boundingRect() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
-    void setSize(qreal size) { m_size = size; }
     void setPosition(const QPointF &position);
     void setColor(const QColor &color) { m_pen.setColor(color); update(); }
 
 private:
-    qreal m_size = 20.0; // Default size of the crosshair arms
+    static const QBitmap& getPatternBitmap();
+
     QPointF m_position;
     QPen m_pen;
 };
